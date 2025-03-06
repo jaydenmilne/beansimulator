99ec7f32f48255caf6b3da22d0a62ca1b7036fc0
Jayden Milne <jaydenmilne@users.noreply.github.com>
2025-03-06 14:35:39 -0700

    More margins

--- docs/index.service.worker.js
+++ docs/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1741296683|2255152953';
+const CACHE_VERSION = '1741296906|2478705453';
 /** @type {string} */
 const CACHE_PREFIX = 'Bean Simulator 2-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
